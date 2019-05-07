91f06c95402f12fafacb294dae9649afe581d983
Angel Gonzalez <angel@capeandbay.com>
2019-05-07 10:56:23 -0400

    ServiceProvider

---
+++ src/TruFitDataRepoServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CapeAndBay\TruFitDataRepo;
+
+use Illuminate\Support\ServiceProvider;
+
+class TruFitDataRepoServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
